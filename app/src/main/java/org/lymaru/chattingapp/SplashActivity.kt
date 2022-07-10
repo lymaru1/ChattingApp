22314b31a571c5f7b67939c03a8fe16183961bc0
lymaru1 <lymaru7@gmail.com>
2022-07-10 20:38:41 +0900

    splash 엑티비티 구현 진행

--- app/src/main/java/org/lymaru/chattingapp/SplashActivity.kt
+++ app/src/main/java/org/lymaru/chattingapp/SplashActivity.kt
@@ -2,8 +2,11 @@ package org.lymaru.chattingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class SplashActivity : AppCompatActivity() {
+    var linearLayout: LinearLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
